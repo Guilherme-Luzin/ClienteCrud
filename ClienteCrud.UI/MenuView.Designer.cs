0cef156f16c2efb961750bc5d33c34837467d1b6
Guilherme-Luzin <guilherme.carvalho@inventsoftware.com.br>
2021-12-08 11:48:54 -0300

    correcao de arquivos 2

---
+++ ClienteCrud.UI/MenuView.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System.Windows.Forms;
+
+namespace ClienteCrud.UI
+{
+    partial class MenuView
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mostrar = new System.Windows.Forms.DataGridView();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExc = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mostrar
+            // 
+            this.mostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mostrar.Location = new System.Drawing.Point(11, 11);
+            this.mostrar.Name = "mostrar";
+            this.mostrar.RowTemplate.Height = 25;
+            this.mostrar.Size = new System.Drawing.Size(778, 390);
+            this.mostrar.TabIndex = 10;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFechar.Location = new System.Drawing.Point(12, 408);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(81, 31);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnExc
+            // 
+            this.btnExc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExc.Location = new System.Drawing.Point(702, 408);
+            this.btnExc.Name = "btnExc";
+            this.btnExc.Size = new System.Drawing.Size(86, 31);
+            this.btnExc.TabIndex = 8;
+            this.btnExc.Text = "Excluir";
+            this.btnExc.UseVisualStyleBackColor = true;
+            this.btnExc.Click += new System.EventHandler(this.btnExc_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEdit.Location = new System.Drawing.Point(618, 409);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(78, 30);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(531, 409);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAdd.Size = new System.Drawing.Size(81, 31);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // MenuView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mostrar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExc);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "MenuView";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.MenuView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mostrar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView mostrar;
+        private Button btnFechar;
+        private Button btnExc;
+        private Button btnEdit;
+        private Button btnAdd;
+    }
+}
